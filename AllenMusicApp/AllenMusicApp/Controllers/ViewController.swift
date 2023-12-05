e931e16c615754db432dde842c278bb9ac9db192
byunsp3610@gmail.com <byunsp3610@gmail.com>
2023-12-05 20:54:21 +0900

    Feat: TableView Cell 등록

--- AllenMusicApp/AllenMusicApp/Controllers/ViewController.swift
+++ AllenMusicApp/AllenMusicApp/Controllers/ViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 class ViewController: UIViewController {
     
     private var mainView = MainView()
+    var music: [Music] = Music.dummyArray
     
-    
-    override func loadView() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         view = mainView
         
         navigationItem.title = "Music Search"
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        setupTableView()
     }
+    
+    func setupTableView() {
+        mainView.tableView.dataSource = self
+        mainView.tableView.register(TableViewCell.self, forCellReuseIdentifier: "MusicCell")
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(#function)
+        return music.count
         
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(#function)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicCell", for: indexPath) as! TableViewCell
+        
+        cell.imageUrl = music[indexPath.row].imageUrl
+        cell.albumNameLabel.text = music[indexPath.row].albumName
+        cell.artistNameLabel.text = music[indexPath.row].artistName
+        cell.releaseDateLabel.text = music[indexPath.row].releaseDateString
+        cell.songNameLabel.text = music[indexPath.row].songName
+        
+        return cell
+    }
 }
 
---
+++ AllenMusicApp/AllenMusicApp/Models/Music.swift
@@ -0,0 +1,8 @@
+//
+//  Musci.swift
+//  AllenMusicApp
+//
+//  Created by 변상필 on 12/5/23.
+//
+
+import Foundation
